e9a116be2971d9741ada1182db009476814359e8
smokilox <ricchhii@yandex.ru>
2025-12-19 18:55:39 +0300

    Add files via upload

---
+++ dishes.js
@@ -0,0 +1,17 @@
+// dishes.js
+const dishes = [
+  // Супы
+  { keyword: 'gazpacho', name: 'Гаспачо', price: 195, category: 'soup', count: '350 г', image: 'maxresdefault.jpg' },
+  { keyword: 'pea_soup', name: 'Гороховый суп с копчёностями', price: 140, category: 'soup', count: '300 мл', image: 'Ham-and-split-pea-soup_6-net-carbs.png' },
+  { keyword: 'mushroom_soup', name: 'Сливочный грибной суп', price: 160, category: 'soup', count: '300 мл', image: 'tW4qfZD68iUQSLCFvCYAbZqPxbJpgkXu.png' },
+
+  // Главные блюда
+  { keyword: 'chicken', name: 'Курица гриль с овощами', price: 350, category: 'main', count: '350 г', image: 'maxresdefault (1).jpg' },
+  { keyword: 'salmon', name: 'Лосось с бурым рисом и терияки', price: 420, category: 'main', count: '300 г', image: 'lazy_smart_crop_1280x720.png' },
+  { keyword: 'pasta', name: 'Паста Болоньезе с пармезаном', price: 380, category: 'main', count: '350 г', image: '_kwt3pgi70bbtx2b_1675415921807_Karamba cover.jpg' },
+
+  // Напитки
+  { keyword: 'orange', name: 'Апельсиновый сок', price: 120, category: 'drink', count: '300 мл', image: '14-5.jpg' },
+  { keyword: 'apple', name: 'Яблочный сок', price: 90, category: 'drink', count: '300 мл', image: '1000x1000__applejuice-1618198577366.jpg' },
+  { keyword: 'carrot', name: 'Морковный сок', price: 110, category: 'drink', count: '300 мл', image: '2-5.jpg' }
+];
